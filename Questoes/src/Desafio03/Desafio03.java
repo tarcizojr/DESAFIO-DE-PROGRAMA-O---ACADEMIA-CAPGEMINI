5c3545bf8e93649e39e9c1307ab3ab6931b38c75
Tarcizo Leite <tarcizoleitemonteiro@gmail.com>
2022-02-18 21:46:38 -0300

    Questoẽs Concluidas

--- Questoes/src/Desafio03/Desafio03.java
+++ Questoes/src/Desafio03/Desafio03.java
@@ -1,31 +1,43 @@
 package Desafio03;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Desafio03 {
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System .in);
 		
+		//Entrada da palavra digitado pelo usuario
 		System.out.println("Qual a Palavra");
 		String palavra = leitor.nextLine();
 		
-		String[] lista = qtdAnagrama(palavra);
-		System.out.println(lista.length);
+		qtdAnagrama(palavra);
+
 	}
-	
+	//Contador de Anagramas
+	public static void qtdAnagrama(String palavra) {
+		//Lista para armazenar os anagramas
+		ArrayList<String> lista = new ArrayList<String>();
+		/*Nessa sequencia de for, ele faz com que seja percorida a palavra de dos geitos destintos, pegando uma letra da palavra e a sua posterior e as comparando*/
+	    for(int i = 0; i < palavra.length(); i++) {
+	        for(int j = i+1; j < palavra.length(); j++) {
+				/*Neste primeiro if, é verificado se as letras das posiçoes i e j são iguais, caso seja verdadeiro e o valor de i seja maior que o valor j, passamos para o segundo if*/
+	            if(palavra.charAt(i) == palavra.charAt(j) && (i <j)) {        
+					/*No segundo if é verificado se a letra da possição i é diferente da letra da posição j, caso sim as duas letras são assumidas como anagrama, e me todos os demais casos as letras das posiçoes i e sua seguinte i+1 tambem são consideradas anagrama  */
+	                if(palavra.charAt(i) != palavra.charAt(i+1)) {              
+	                  String anagrama = palavra.substring(i, j);
+	                  lista.add(anagrama);
 
-	public static String[] qtdAnagrama(String palavra) {
-		String[] lista = new String[palavra.length()];
+	                }
+					String anagrama = palavra.substring(i, i + 1);
+					lista.add(anagrama);
+	            }
+	        }
+	    }
 
-		if (palavra.length() >= 1) { 
-			for (int i = 0; i < palavra.length(); i++) {				
-				String anterior = palavra.substring(0, i); 
-				String posterior = palavra.substring(i + 1); 
-				qtdAnagrama(anterior + posterior);				
-			}
-		}
-	
-		return lista;
+	/*Os anagramas identificados são colocados dentro de um ArrayList e para sabermos quantos anagramas temos, basta imprimir o tamanho desse ArrayList */
+		System.out.println(lista.size());
+		
 	}
 
 }
--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Esta questão funciona da sequinte forma, ao executar o programa,
 
 # Questão 2 (Validador de Senha)
 Esta segunda questão corresponde a um validador de senha, quando execudado pede para o ususrio digitar uma senha par verificar se a mesma é segura, para fazer esta verificação é ultilizada a classe ValidadorDeSenha, sela esta todo o processo para verificar a segurança da senha, seguindo alguns criterios pre estabelecidos, eles são, a senha deve ter no minimo 6 caracters de tamanho, uma letra maiuscula e uma menuscula, e tambem um numeral e um caracter. Caso a senha tenha todas estas caracteristicas ela é considerada segura, caso contrario ela é considerada insegura e é retornado ao usuario o motivo, como ela ser muito curta ou precisar de um numeral.
+
+# Questão 3
+Essa questão tem como objetivo informar a quantidade de anagramas de um determinada palavra que é informada pelo ussuario, recebendo essa palavra o codigo verifica as letras de deremidas posiçoes e as comparar com outras de posiçoes seguintes para indendificar se formam ou não um anagrama. 
